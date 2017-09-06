c908a2ebd1bf6599075924d8c98e9bb67b8f532e
gazwald <gary.brandon@gmail.com>
2017-09-07 09:40:21 +1000

    Moved database handling into functions Renamed s (session) to db for consistency

--- app.py
+++ app.py
@@ -2,23 +2,19 @@
 import json
 
 from database.orm import Server, Status
-
-from sqlalchemy import create_engine, and_
-from sqlalchemy.orm import sessionmaker
+from database.functions import create_session
 
 from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
 
 
 def build_serverlist(game='q2'):
-    engine = create_engine('postgresql://q2master:password@192.168.124.86:5432/q2master')
-    Session = sessionmaker(bind=engine)
-    s = Session()
+    db = create_session()
 
     str_fmt = '%Y-%m-%d %H:%M'
 
     serverlist = []
-    for server in s.query(Status).join(Server).filter(Server.active == True).all():
+    for server in db.query(Status).join(Server).filter(Server.active == True).all():
         first_seen = server.server.first_seen
         last_seen = server.server.last_seen
         serverdict = server.__dict__
@@ -30,7 +26,7 @@ def build_serverlist(game='q2'):
         serverdict['last_seen'] = last_seen.strftime(str_fmt)
         serverdict['_sa_instance_state'] = None
         serverlist.append(serverdict)
-    s.close()
+    db.close()
     return serverlist
 
 app = Flask(__name__)
